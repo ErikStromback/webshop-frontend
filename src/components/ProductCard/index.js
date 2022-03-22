cf26733ec969e6678610001596874c59c0bca64a
churris-x <francisco@altimari.nl>
2022-03-22 11:52:10 +0100

    Implemented product card.

--- src/components/ProductCard/index.js
+++ src/components/ProductCard/index.js
@@ -1,5 +1,26 @@
+import { AiOutlineHeart, AiOutlineUser } from 'react-icons/ai';
+import { FiShoppingCart } from 'react-icons/fi';
+import './styles.css';
+
 const ProductCard = ({title, price, rating, description, mainImage}) => {
-	return <h3>{title}</h3>
+	return (
+		<div className="ProductCard-container">
+			<img />
+			<div>
+				<h3>{title}</h3>
+				<div>
+					<h3>{price}</h3>
+					<h3>{rating}</h3>
+				</div>
+				<p>{description}</p>
+				<div>
+					<AiOutlineHeart />
+					<AiOutlineUser />
+					<FiShoppingCart />
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export { ProductCard };
--- src/pages/ShopPage/index.js
+++ src/pages/ShopPage/index.js
@@ -1,14 +1,21 @@
+import { useState, useEffect} from 'react';
 import { ProductCard } from '../../components';
+import './styles.css';
 
 const ShopPage = () => {
 	
+	const [products, setProducts] = useState([]);
 	const mockProducts = [{title: 'hello'}, {title: 'world'}]
 	
+	useEffect(() => {
+
+	},[]);
+
 	return (
 	<div className="ShopPage-container">
 		<div className="ShopPage-categories"></div>
 		<div className="ShopPage-product-feed">
-			{mockProducts.map(item => 
+			{mockProducts.length !== 0 && mockProducts.map(item => 
 				<ProductCard {...item}/>
 			)}
 		</div>
