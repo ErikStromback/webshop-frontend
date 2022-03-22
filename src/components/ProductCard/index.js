0753b22c7135d3886b81f468deb8034f6803cf88
churris-x <francisco@altimari.nl>
2022-03-22 11:22:16 +0100

    Added product card component.

---
+++ src/components/ProductCard/index.js
@@ -0,0 +1,5 @@
+const ProductCard = ({title, price, rating, description, mainImage}) => {
+	return <h3>{title}</h3>
+};
+
+export { ProductCard };
--- src/pages/ShopPage/index.js
+++ src/pages/ShopPage/index.js
@@ -1,5 +1,19 @@
+import { ProductCard } from '../../components';
+
 const ShopPage = () => {
-  return <h1>ShopPage</h1>;
+	
+	const mockProducts = [{title: 'hello'}, {title: 'world'}]
+	
+	return (
+	<div className="ShopPage-container">
+		<div className="ShopPage-categories"></div>
+		<div className="ShopPage-product-feed">
+			{mockProducts.map(item => 
+				<ProductCard {...item}/>
+			)}
+		</div>
+	</div>
+  );
 };
 
 export default ShopPage;
