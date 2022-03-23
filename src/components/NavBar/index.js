23742bf92a1c6f02a5d51c9f4293c91cc61c5179
ErikStromback <96985307+ErikStromback@users.noreply.github.com>
2022-03-23 11:28:23 +0100

    added details route to navbar

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,9 +1,8 @@
 import "./styles.css";
 import { NavLink } from "react-router-dom";
 
-import { AiOutlineHeart, AiOutlineUser } from 'react-icons/ai';
-import { FiSearch, FiShoppingCart } from 'react-icons/fi';
-
+import { AiOutlineHeart, AiOutlineUser } from "react-icons/ai";
+import { FiSearch, FiShoppingCart } from "react-icons/fi";
 
 const NavBar = () => {
   return (
@@ -45,16 +44,24 @@ const NavBar = () => {
           >
             Shop
           </NavLink>
+          <NavLink
+            to="/details/2"
+            style={({ isActive }) =>
+              isActive ? { color: "white" } : undefined
+            }
+          >
+            Details
+          </NavLink>
         </div>
         <div className="NavBar-searchBar">
           <span>{""}</span>
-		  <FiSearch  style={{color: '#9295AA', margin: 8}}/>
+          <FiSearch style={{ color: "#9295AA", margin: 8 }} />
         </div>
       </div>
       <div className="NavBar-container-icons">
-		<AiOutlineUser  size={17} style={{ color: 'white' }}/>
-		<FiShoppingCart size={17} style={{ color: 'white' }}/>
-		<AiOutlineHeart size={17} style={{ color: 'white' }}/>
+        <AiOutlineUser size={17} style={{ color: "white" }} />
+        <FiShoppingCart size={17} style={{ color: "white" }} />
+        <AiOutlineHeart size={17} style={{ color: "white" }} />
       </div>
     </div>
   );
